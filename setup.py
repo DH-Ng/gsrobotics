5c5d01cc8f5be05855091b1b0d6d5558e48a5325
DH-Ng <duc_huy.nguyen@stud.tu-darmstadt.de>
2026-01-12 14:16:41 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with (Path(__file__).resolve().parent / "VERSION").open() as f:
 setup(
     name="gelsight",
     package_dir={"": "src"},
-    packages=["gelsight"],
+    packages=find_packages(),
     author="",
     maintainer="",
     url="https://github.com/DH-Ng/gsrobotics",
